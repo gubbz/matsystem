30402abcd14c03751e18ea9dbef429a5ff833c6e
Jyborn <Alexander.jyborn@gmail.com>
2019-03-22 17:11:08 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ import Statistics from './components/Statistics.js'
 import QuestionView from './components/QuestionView.js'
 import socketIOClient from 'socket.io-client'
 
-const socketURL = "localhost:8080";
+const socketURL = "/";
 class App extends Component {
 
   constructor() {

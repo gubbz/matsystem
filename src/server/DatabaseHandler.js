66976ce90ee33766919c41c0c4561361f4d5e1d3
Jyborn <alexander.jyborn@gmail.com>
2019-05-15 23:37:53 +0200

    fixa

--- src/server/DatabaseHandler.js
+++ src/server/DatabaseHandler.js
@@ -80,6 +80,7 @@ module.exports = class DatabaseHandler {
       text: 'SELECT * FROM "subQuestions" WHERE date_fk = ($1)',
       values: [today]
     }
+
     this.con.query(query2, (err, res) => {
       if (err) {
         console.log(err.stack);
@@ -305,57 +306,57 @@ module.exports = class DatabaseHandler {
     var query, query2;
     var currentDate = new Date().toISOString().substring(0, 10);
     var x;
-    for (var i = 0; i < this.currentSubVotes.length; i++) {
+    /*for (var i = 0; i < this.currentSubVotes.length; i++) {
       if (this.currentSubVotes[i]['question'] == this.question) {
           x = i;
       }
-    }
+    }*/
 
     switch(typeOfVote)  {
       case "very_bad":
         currentVote = parseInt(this.currentVotes[3], 10) + 1;
-        currentSubVote = Number(this.currentSubVotes[x]['v_bad']) + 1;
+        //currentSubVote = Number(this.currentSubVotes[x]['v_bad']) + 1;
         this.currentVotes[3] = currentVote;
-        this.currentSubVotes[3] = currentSubVote;
+        //this.currentSubVotes[3] = currentSubVote;
 
 
         query = "UPDATE grades SET very_bad = ($1) WHERE date_pk = ($2)";
-        if(this.question != ""){
+        /*if(this.question != ""){
           query2 = "UPDATE subQuestions SET v_bad = ($3) WHERE date_fk = ($2) AND question = ($4)"
-        }
+        }*/
         console.log("currentvote: " + currentVote);
         console.log("query i switchen: " + query);
         break;
       case "bad":
         currentVote = parseInt(this.currentVotes[2], 10) + 1;
-        currentSubVote = Number(this.currentSubVotes[x]['bad']) + 1;
+        //currentSubVote = Number(this.currentSubVotes[x]['bad']) + 1;
         this.currentVotes[2] = currentVote;
-        this.currentSubVotes[2] = currentSubVote;
-        if(this.question != ""){
+        //this.currentSubVotes[2] = currentSubVote;
+        /*if(this.question != ""){
           query2 = "UPDATE subQuestions SET bad = ($3) WHERE date_fk = ($2) AND question = ($4)"
-        }
+        }*/
         query = "UPDATE grades SET bad = ($1) WHERE date_pk = ($2)";
         console.log("currentvote: " + currentVote);
         break;
       case "good":
         currentVote = parseInt(this.currentVotes[1], 10) + 1;
-        currentSubVote = Number(this.currentSubVotes[x]['good']) + 1;
+        //currentSubVote = Number(this.currentSubVotes[x]['good']) + 1;
         this.currentVotes[1] = currentVote;
-        this.currentSubVotes[1] = currentSubVote;
-        if(this.question != ""){
+        //this.currentSubVotes[1] = currentSubVote;
+        /*if(this.question != ""){
           query2 = "UPDATE subQuestions SET good = ($3) WHERE date_fk = ($2) AND question = ($4)"
-        }
+        }*/
         query = "UPDATE grades SET good = ($1) WHERE date_pk = ($2)";
         console.log("currentvote: " + currentVote);
         break;
       case "very_good":
         currentVote = parseInt(this.currentVotes[0], 10) + 1;
-        currentSubVote = Number(this.currentSubVotes[x]['v_good']) + 1;
+        //currentSubVote = Number(this.currentSubVotes[x]['v_good']) + 1;
         this.currentVotes[0] = currentVote;
-        this.currentSubVotes[0] = currentSubVote;
-        if(this.question != ""){
+        //this.currentSubVotes[0] = currentSubVote;
+        /*if(this.question != ""){
           query2 = "UPDATE subQuestions SET v_good = ($3) WHERE date_fk = ($2) AND question = ($4)"
-        }
+        }*/
         query = "UPDATE grades SET very_good = ($1) WHERE date_pk = ($2)";
         console.log("currentvote: " + currentVote);
         break;
@@ -373,13 +374,13 @@ module.exports = class DatabaseHandler {
       }
     });
 
-    this.con.query(query2, values, (err, res) => {
+    /*this.con.query(query2, values, (err, res) => {
       if(err){
         console.log(err.stack);
       } else {
         console.log("grades + 1 successful");
       }
-    });
+    });*/
  }
 
   startOfWeek(date) {
